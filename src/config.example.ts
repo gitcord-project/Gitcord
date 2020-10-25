51f3e5425a28aa37ce729892050e5f8e5277e3b2
Charalampos Fanoulis <yo@fanoulis.dev>
2020-10-25 16:09:13 +0200

    fix: include example config

---
+++ src/config.example.ts
@@ -0,0 +1,18 @@
+// Remove `.example` from the file extension to configure Evlyn
+import { LogLevel } from '@sapphire/framework';
+import { ClientOptions } from 'discord.js';
+
+export const PREFIX = 'gh!';
+
+export const CLIENT_OPTIONS: ClientOptions = {
+	dev: true,
+	ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] },
+	messageCacheLifetime: 120,
+	messageCacheMaxSize: 25,
+	messageSweepInterval: 300,
+	presence: { activity: { name: 'with GitHub issues', type: 'PLAYING' } },
+	logger: { level: LogLevel.Debug },
+	restTimeOffset: 0
+};
+
+export const TOKEN = '';
