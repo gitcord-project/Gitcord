b6a183b95225fb0f7a1af0fe54529fa50d245a1a
Charalampos Fanoulis <yo@fanoulis.dev>
2020-11-02 12:49:01 +0200

    feat: prettify the help command

--- src/commands/Info/help.ts
+++ src/commands/Info/help.ts
@@ -1,13 +1,12 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Args, Command, CommandOptions } from '@sapphire/framework';
-import { Store } from '@sapphire/pieces';
-import { Message } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
 	aliases: ['h'],
 	description: 'Gives you a list of commands',
 	detailedDescription: 'You may also provide a command, which will return info about that command',
-	preconditions: ['OwnerOnly']
+	preconditions: []
 })
 export default class Help extends Command {
 	public async run(message: Message, args: Args) {
@@ -17,10 +16,19 @@ export default class Help extends Command {
 	}
 
 	private async commandHelp(message: Message, cmd: string) {
-		return message.channel.send(cmd);
+		const command = this.client.commands.get(cmd);
+		if (typeof command === 'undefined') return message.channel.send("Couldn't find that command!");
+		return message.channel.send(
+			new MessageEmbed()
+				.setColor(0x1100ff)
+				.setTitle(command.name)
+				.setDescription(`${command.description}`)
+				.addField('\u200B', '\u200B')
+				.addField('In detail:', command.detailedDescription)
+		);
 	}
 
 	private mapCommandsToStr() {
-		return ((this.store as unknown) as Store<Command>).map((val) => `${val.name} → ${val.description}`).join('\n');
+		return this.client.commands.map((val) => `${val.name} → ${val.description}`).join('\n');
 	}
 }
