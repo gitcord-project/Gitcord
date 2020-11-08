3575347074d95efa035bda7adca96ce2a434b74a
Charalampos Fanoulis <yo@fanoulis.dev>
2020-11-08 14:53:04 +0200

    feat: add apiclient in bot client

--- src/lib/GitcordClient.ts
+++ src/lib/GitcordClient.ts
@@ -1,14 +1,17 @@
 /* eslint-disable @typescript-eslint/no-invalid-this */
-import { PREFIX } from '@root/config';
+import { GH_TOKEN, PREFIX } from '@root/config';
 import { SapphireClient } from '@sapphire/framework';
 import { ClientOptions } from 'discord.js';
+import { GithubApi } from './structures/GithubApi';
 
 export class GitcordClient extends SapphireClient {
+	public github: GithubApi;
 	public constructor({ dev = false, ...options }: ClientOptions) {
 		super({
 			...options,
 			dev
 		});
+		this.github = new GithubApi(GH_TOKEN);
 		this.registerUserDirectories();
 	}
 
