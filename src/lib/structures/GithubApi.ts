ba1b3d53f684a091c3d615233614888d44fe9357
Charalampos Fanoulis <yo@fanoulis.dev>
2020-11-08 14:48:41 +0200

    refactor: make apiclient use tokens instead of ghapps auth

--- src/lib/structures/GithubApi.ts
+++ src/lib/structures/GithubApi.ts
@@ -1,7 +1,7 @@
 import { GraphqlVariables } from '@lib/types/Types';
 import { fetch } from '@utils/util';
-import { readFileSync } from 'fs';
-import { sign } from 'jsonwebtoken';
+// import { readFileSync } from 'fs';
+// import { sign } from 'jsonwebtoken';
 
 export class GithubApi {
 	/**
@@ -12,39 +12,43 @@ export class GithubApi {
 	/**
 	 * The application's ID, as provided by GitHub
 	 */
-	public applcationId: string;
+	// public applcationId: string;
 
-	private jwt?: string;
+	// private jwt?: string;
 
-	private jwtExpiry?: number;
+	// private jwtExpiry?: number;
 
-	private jwtPrivateKey: string;
+	// private jwtPrivateKey: string;
 
-	public constructor(applicationId: string, keyPath: string) {
-		this.applcationId = applicationId;
-		this.jwtPrivateKey = readFileSync(keyPath, 'utf-8');
+	private token: string;
+
+	// public constructor(applicationId: string, keyPath: string) {
+	public constructor(token: string) {
+		// this.applcationId = applicationId;
+		// this.jwtPrivateKey = readFileSync(keyPath, 'utf-8');
+		this.token = token;
 	}
 
 	/**
 	 * Github apps JWT, used to autenticate against GH as the *app*, **not an installation**
 	 */
-	public get key() {
-		if (this.jwtExpiry && Date.now() < this.jwtExpiry) return this.jwt;
-		this.jwt = this.generateJwtKey();
-		return this.jwt;
-	}
+	// public get key() {
+	// 	if (this.jwtExpiry && Date.now() < this.jwtExpiry) return this.jwt;
+	// 	this.jwt = this.generateJwtKey();
+	// 	return this.jwt;
+	// }
 
 	/**
 	 * Signs a new JWT
 	 */
-	public generateJwtKey() {
-		this.jwtExpiry = Date.now() + 9 * 60;
-		return sign({}, this.jwtPrivateKey, {
-			algorithm: 'RS256',
-			issuer: this.applcationId,
-			expiresIn: this.jwtExpiry
-		});
-	}
+	// public generateJwtKey() {
+	// 	this.jwtExpiry = Date.now() + 9 * 60;
+	// 	return sign({}, this.jwtPrivateKey, {
+	// 		algorithm: 'RS256',
+	// 		issuer: this.applcationId,
+	// 		expiresIn: this.jwtExpiry
+	// 	});
+	// }
 
 	/**
 	 * Run a GraphQL query against Github
@@ -52,13 +56,13 @@ export class GithubApi {
 	 * @param variables Owner, repo and issue/PR number variables
 	 * @param key Installation token
 	 */
-	public runQuery(query: string, variables: GraphqlVariables, key: string) {
-		return fetch(this.url, {
+	public runQuery<T = Record<string, unknown>>(query: string, variables: GraphqlVariables) {
+		return fetch<T>(this.url, {
 			body: JSON.stringify({ query, variables }),
 			headers: {
 				'User-Agent': 'GitcordBot/v0.0.1 by @cfanoulis',
 				Accept: 'application/vnd.github.v3+json',
-				Authentication: `Bearer ${key}`
+				Authentication: `Bearer ${this.token}`
 			}
 		});
 	}
@@ -70,10 +74,10 @@ export class GithubApi {
 		issueOrPR: `query issueOrPr($repository: String!, $owner: String!, $number: Int!) {
 						repository(owner: $owner, name: $repository) {
 							issue(number: $number) {
-								id
+								number
 							}
 							pullRequest(number: $number) {
-								id
+								number
 							}
 						}
 					}`,
